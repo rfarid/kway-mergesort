1e51312d13f7589f3f3b2bd5a05c008abbd4380f
Reza <rezaf@cse>
2017-05-14 13:11:09 +1000

    first commit adding README_RF.txt and StringProcessor single and multi threaded versions

---
+++ README_RF.txt
@@ -0,0 +1,24 @@
+This is a modified version
+
+Source: https://github.com/arq5x/kway-mergesort
+
+kwaymergesort_single_thread.h has the additional items as below:
+    displaying a progress bar
+    calling expensiveFunc() per each line read from the file
+
+Memory Usage:
+    maxBufferSize defined by user
+
+Speed:
+    This is an external memory sort-merge example.
+    So, to analyse the speed and time complexity, we consider main/external memory exchange.
+    General strategy
+        Break the data into blocks about the size of the internal memory
+        Sort these blocks
+        Merge sorted blocks 
+    Usually several passes are needed, creating larger sorted blocks until the whole file is sorted
+    Time complexity: O(log (N/B)) in which N is the total size and B is the memory size
+
+How to Compile/Run:
+    g++ StringProcessorSingleThread.cpp -o testsort
+    ./testsort 0 english.txt
---
+++ StringProcessorMultiThread.cpp
@@ -0,0 +1,34 @@
+// String processor (single-threaded)
+// The base of this code is kway-mergesort from https://github.com/arq5x/kway-mergesort
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <math.h>
+using namespace std;
+
+// local includes
+#include "kwaymergesort_multi_thread.h"
+int  bufferSize     = 100000;      // allow the sorter to use 100Kb (base 10) of memory for sorting.  
+                                   // once full, it will dump to a temp file and grab another chunk.     
+char outFile[]       = "out_sorted.txt";
+// comparison functions for the ext. merge sort	
+bool alphaAsc(const string &a, const string &b) { return a < b; }
+
+int main(int argc, char* argv[]) {
+    int delaySec        = atoi(argv[1]);
+    string inFile       = argv[2];
+    bool compressOutput = false;       // not yet supported
+    string tempPath     = "./";        // allows you to write the intermediate files anywhere you want.
+    ofstream outputFile(outFile);
+    // sort the lines of a file lexicographically in ascending order (akin to UNIX sort, "sort FILE")
+    KwayMergeSort<string> *sorter = new KwayMergeSort<string> (inFile, 
+                                                               delaySec,      
+                                                               &outputFile, 
+                                                               alphaAsc, 
+                                                               bufferSize, 
+                                                               compressOutput, 
+                                                               tempPath);
+    sorter->Sort();
+}
